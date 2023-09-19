db9df2fd580529c32fe06640f3eb1e6c25c0ee93
Dan9el8 <mutetemifloice@gmail.com>
2023-09-19 04:16:35 -0700

    simple shell

--- error.c
+++ error.c
@@ -13,8 +13,8 @@ size_t len;
 ssize_t num_written;
 
 len = _strlen(str);
-num_written = write(STDOUT-FILENO, str, len);
-if (num_written = -1)
+num_written = write(STDOUT_FILENO, str, len);
+if (num_written == -1)
 {
 perror( "write");
 }
